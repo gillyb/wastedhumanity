a7cd5e98c3a086225563b96ccf548ed0d634386c
Gilly Barr <gilly@logz.io>
2017-07-29 13:39:13 +0300

    Use request module for nodejs requests

--- GAMEPLAN.md
+++ GAMEPLAN.md
@@ -1,7 +1,7 @@
 ### GAMEPLAN
 
 - [x] Update all modules
-- [] Lock module versions
+- [x] Lock module versions
 - [] Remove 'how much time' button
 - [] Display time details of video without having to push button
 - [] Create internal cache for video calculations
--- youTubeProvider.js
+++ youTubeProvider.js
@@ -1,78 +1,90 @@
+const https = require('https');
+const request = require('request');
+const Promise = require('bluebird');
 
-var https = require('https');
 
 var youtubeApiKey = 'AIzaSyA0xWbm5Q7SF5aFZQJ8EGmb6fNc8cjQEwg';
 
-var _getVideoId = function(url) {
+var _getVideoId = function (url) {
 
-	var videoid = url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
-	if (videoid != null) {
-		if (videoid.indexOf('v=') > -1) {
-			console.log('fixing youtube id');
-			console.log(videoid.substr(2));
-			return videoid.substr(2);
-		}
+    var videoid = url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+    if (videoid != null) {
+        if (videoid.indexOf('v=') > -1) {
+            console.log('fixing youtube id');
+            console.log(videoid.substr(2));
+            return videoid.substr(2);
+        }
 
-		return videoid[1];
-	}
-	
-	return null;
+        return videoid[1];
+    }
+
+    return null;
 };
 
-var _getVideoInfo = function(videoId, callback) {
+var _getTrendingVideos = (numResults) => {
 
-	var options = {
-		'method': 'GET',
-		'host': 'www.googleapis.com',
-		'port': 443,
-		'path': '/youtube/v3/videos?part=contentDetails,statistics,snippet&id=' + videoId + '&key=' + youtubeApiKey
-	};
-	
-	var data = '';
-	console.log('making request to :' + options.path);
-	https.request(options, function(res) {
-		res.on('data', function(d) {
-			data += d;
-		});
-		res.on('end', function() {
-			try {
-				var videoData = extractVideoData(data);
-				callback(videoData);
-			}
-			catch (e) {
-				console.log('error extracting video data. Error: ' + e);
-				console.log(data);
-				callback(null);
-			}
-		});
-	})
-	.on('error', function(er) {
-		console.log(er);
-		callback(null);
-	})
-	.end();
+    return new Promise((resolve, reject) => {
+        request({
+            uri: 'https://www.googleapis.com/youtube/v3/videos?part=contentDetails,statistics,snippet&chart=mostPopular&key=' + youtubeApiKey + '&maxResults=' + numResults,
+            method: 'GET',
+            json: true
+        }, (err, response, body) => {
+
+            if (err || response.statusCode !== 200) {
+                reject();
+                return;
+            }
+            let videos = body.items.map((video) => extractVideoData(video));
+            resolve(videos);
+
+        });
+    });
+
 };
 
-function extractVideoData(data) {
-	var jsonObj = JSON.parse(data.toString());
-	var videoObj = jsonObj['items'][0];
+var _getVideoInfo = function (videoId, callback) {
 
-	var videoId = videoObj['id'];
-	var videoName = videoObj['snippet']['title'];
-	var videoLength = videoObj['contentDetails']['duration'];
-	var videoViewCount = videoObj['statistics']['viewCount'];
-	var videoDefaultThumbnail = videoObj['snippet']['thumbnails']['default']['url'];
-	var videoMediumThumbnail = videoObj['snippet']['thumbnails']['medium']['url'];
+    console.log('making request to youtube video api');
 
-	return {
-		id: videoId,
-		title: videoName.replace('\'', '\\\'').replace('"', '\"'),
-		length: videoLength.replace('\'', '\\\'').replace('"', '\"'),
-		views: videoViewCount.replace('\'', '\\\'').replace('"', '\"'),
-		thumbnail: videoDefaultThumbnail.replace('\'', '\\\'').replace('"', '\"'),
-		bigThumbnail: videoMediumThumbnail.replace('\'', '\\\'').replace('"', '\"')
-	};
+    request({
+        uri: 'https://www.googleapis.com/youtube/v3/videos?part=contentDetails,statistics,snippet&id=' + videoId + '&key=' + youtubeApiKey,
+        method: 'GET',
+        json: true
+    }, (err, response, body) => {
+        if (err) {
+            console.log('Error while fetching youtube video data');
+            callback(null);
+        }
+
+        try {
+            var videoData = extractVideoData(body.items[0]);
+            callback(videoData);
+        }
+        catch (e) {
+            console.log('error extracting video data. Error: ' + e);
+            callback(null);
+        }
+    });
+};
+
+function extractVideoData(videoObj) {
+    var videoId = videoObj['id'];
+    var videoName = videoObj['snippet']['title'];
+    var videoLength = videoObj['contentDetails']['duration'];
+    var videoViewCount = videoObj['statistics']['viewCount'];
+    var videoDefaultThumbnail = videoObj['snippet']['thumbnails']['default']['url'];
+    var videoMediumThumbnail = videoObj['snippet']['thumbnails']['medium']['url'];
+
+    return {
+        id: videoId,
+        title: videoName.replace('\'', '\\\'').replace('"', '\"'),
+        length: videoLength.replace('\'', '\\\'').replace('"', '\"'),
+        views: videoViewCount.replace('\'', '\\\'').replace('"', '\"'),
+        thumbnail: videoDefaultThumbnail.replace('\'', '\\\'').replace('"', '\"'),
+        bigThumbnail: videoMediumThumbnail.replace('\'', '\\\'').replace('"', '\"')
+    };
 }
 
 exports.getVideoId = _getVideoId;
 exports.getVideoInfo = _getVideoInfo;
+exports.getTrendingVideos = _getTrendingVideos;
